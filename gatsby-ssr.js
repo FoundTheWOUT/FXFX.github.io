43f1708ea5dea099f130c761c3ab980124e00816
FoundTheWOUT <waua@outlook.com>
2022-09-27 21:58:23 +0800

    Fix theme flicker first load

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,3 +4,27 @@ import Layout from "./src/components/Layout";
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents(
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        try{
+          const mql = window.matchMedia('(prefers-color-scheme: dark)');
+          const colorScheme = localStorage['user-color-scheme']
+          if (colorScheme === "dark" || (colorScheme === "system" && mql.matches) || (!colorScheme && mql.matches)) {
+            document.documentElement.classList.add("dark");
+          } else {
+            document.documentElement.classList.remove("dark");
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      `,
+        }}
+      />
+    </>
+  );
+};
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import React, { PropsWithChildren, useEffect, useState } from "react";
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
-// import BackGround from "@/components/BackGround";
 import { FaMonument } from "react-icons/fa";
 import Footer from "@/components/Footer";
 import { NightSwitchContext, TColorScheme } from "@/components/NightSwitch";
 import { useLocalStorage, useMedia } from "react-use";
-import classNames from "classnames";
 
 const isBrowser = typeof window !== "undefined";
 
@@ -20,26 +18,30 @@ const Layout = (props: PropsWithChildren) => {
     isBrowser ? undefined : true
   );
   /**
-   * TODO: fix flash when client is 'light' mode.
    * default scheme state to 'system', this can prevent mismatch of the SSG output and Client first load.
    * later, in client side, we use the useEffect hook to reset the scheme state depend on localStorage.
    * However, this causing the flashing when client side default to 'light' mode.
    *
    * reference: https://with-svelte.com/lessons/ssr-dark-mode
+   *
+   * 👆👆 outdate
+   * fix: https://www.joshwcomeau.com/react/dark-mode/#introduction
    * */
-  const [localColorScheme, setLocalColorScheme] =
-    useState<TColorScheme>("system");
   const [colorSchemeStorage, setColorSchemeStorage] =
-    useLocalStorage<TColorScheme>("user-color-scheme", "system");
+    useLocalStorage<TColorScheme>("user-color-scheme", "system", { raw: true });
 
   const setColorScheme = (scheme: TColorScheme) => {
     setColorSchemeStorage(scheme);
-    setLocalColorScheme(scheme);
+    if (scheme === "dark" || (scheme === "system" && isSystemDark)) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
   };
 
   useEffect(() => {
-    setColorScheme(colorSchemeStorage);
-  }, [colorSchemeStorage]);
+    colorSchemeStorage === "system" && setColorScheme("system");
+  }, [isSystemDark]);
 
   return (
     <NightSwitchContext.Provider
@@ -51,26 +53,18 @@ const Layout = (props: PropsWithChildren) => {
         },
       }}
     >
-      <div
-        className={classNames({
-          dark:
-            localColorScheme === "dark" ||
-            (localColorScheme === "system" && isSystemDark),
-        })}
-      >
-        <div className="dark:bg-gray-900 min-h-screen">
-          {/* <BackGround /> */}
-          <NavBar show={true} />
-          <SideBar show={showSideBar} setShowSideBar={setShowSideBar} />
-          {props.children}
-          <Footer />
-          {/* float icon */}
-          <div
-            className="md:invisible fixed bottom-0 right-0 m-6 h-14 w-14 rounded-full bg-gradient-to-tr from-gold to-purple-500 flex items-center justify-center shadow-lg active:shadow-inner"
-            onClick={triggerSideBar}
-          >
-            <FaMonument className="text-white text-2xl" />
-          </div>
+      <div className="dark:bg-gray-900 min-h-screen">
+        {/* <BackGround /> */}
+        <NavBar show={true} />
+        <SideBar show={showSideBar} setShowSideBar={setShowSideBar} />
+        {props.children}
+        <Footer />
+        {/* float icon */}
+        <div
+          className="md:invisible fixed bottom-0 right-0 m-6 h-14 w-14 rounded-full bg-gradient-to-tr from-gold to-purple-500 flex items-center justify-center shadow-lg active:shadow-inner"
+          onClick={triggerSideBar}
+        >
+          <FaMonument className="text-white text-2xl" />
         </div>
       </div>
     </NightSwitchContext.Provider>
